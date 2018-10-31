b7294b96411306e04de32d81f75786fa950af703
Tiago Campos <tiago_vampiro1994@hotmail.com>
2018-10-31 11:45:22 +0000

    new update for chat app

--- ChatApp/app/src/main/java/pdm/ipbeja/pt/chatapp/MainActivity.java
+++ ChatApp/app/src/main/java/pdm/ipbeja/pt/chatapp/MainActivity.java
@@ -1,7 +1,17 @@
 package pdm.ipbeja.pt.chatapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +19,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addNewMessageOnCliked(View view) {
+    }
+
+    public class ChatViewHolder extends RecyclerView{
+
+
+        public ChatViewHolder(@NonNull View view) {
+            super(view);
+        }
+
+        public void bind(/*Chat chat*/){
+
+        }
+    }
+
+
+    public class ChatAdpter extends RecyclerView.Adapter<ChatViewHolder>{
+
+        private List<Chat> data = new ArrayList<>();
+
+        @NonNull
+        @Override
+        public ChatViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull ChatViewHolder chatViewHolder, int i) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
